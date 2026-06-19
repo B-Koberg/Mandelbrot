24eec7c4bc80cf50bb3d096ae28bf6ac238917b9
Bastian <b.koberg412@gmail.com>
2026-06-19 17:23:11 +0200

    log files added, png names changed with maxiter,  logartihmix scale added

--- mandelbrot_calc/parameters.f90
+++ mandelbrot_calc/parameters.f90
@@ -2,11 +2,11 @@ module parameters
     implicit none
     public :: nx, ny, max_iter, x_min, x_max, y_min, y_max
 
-    integer, parameter :: ratio_x = 3, ratio_y = 2
-    integer, parameter :: base_size = 1300
+    integer, parameter :: ratio_x = 2, ratio_y = 2
+    integer, parameter :: base_size = 1500
     integer, parameter :: nx = ratio_x * base_size, ny = ratio_y * base_size
     integer, parameter :: max_iter = 150
-    real(8), parameter :: x_min = -2.0d0, x_max = 1.0d0
+    real(8), parameter :: x_min = -2.0d0, x_max = 2.0d0
     real(8), parameter :: y_min = -(x_max - x_min) * (real(ny) / real(nx)) / 2.0d0, y_max = -y_min
 
 end module parameters
--- mandelbrot_calc/plot.py
+++ mandelbrot_calc/plot.py
@@ -17,14 +17,15 @@ fn = "mandelbrot_output.bin"
 with open(fn, "rb") as f:
     header = np.fromfile(f, dtype=np.float64, count=7)
 nx, ny = int(header[0]), int(header[1])
+max_iter = int(header[2])
 header_bytes = 7 * 8  # 7 float64
 
 mm = np.memmap(fn, dtype=np.int32, mode='r', offset=header_bytes, shape=(nx * ny,))
 arr_view = mm.reshape((nx, ny), order='F').T  # arr[y,x]
 
 
-possible_block_h = [ny / i for i in [0.0625, 0.125, 0.25, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]
-min_block_h_idx = np.argmin([abs(bh - 4092) for bh in possible_block_h])
+possible_block_h = [ny / i for i in [0.0625, 0.125, 0.25, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 35]]
+min_block_h_idx = np.argmin([abs(bh - 1000) for bh in possible_block_h])
 block_h = round(possible_block_h[min_block_h_idx]) + 1
 
 print(f"({now_hms()}) Erstelle Bild mit Pixelreihen im Arbeitsspeicher {block_h} von {ny} Pixelreihen insgesamt...")
@@ -37,14 +38,19 @@ for y0 in range(0, ny, block_h):
     if bmax == bmin:
         norm = np.zeros_like(block, dtype=np.float32)
     else:
-        norm = (block - bmin) / (bmax - bmin)
+        #norm = (block - bmin) / (bmax - bmin)
+        norm = np.log(block - bmin + 1) / np.log(bmax - bmin + 1)
+        
     rgb_block = (cm.inferno(norm)[:, :, :3] * 255).astype('uint8')
     img.paste(Image.fromarray(rgb_block), (0, y0))
     del block, norm, rgb_block
     gc.collect()
+    
+    print (f"({now_hms()}) Progress {y1}/{ny} Pixelreihen verarbeitet...")
 
 print(f"({now_hms()}) Bild erstellt, speichere und lösche temporäre Daten...")
-img.save("mandelbrot_color.png")
+img.save(f"mandelbrot_{max_iter}.png")
 del img, arr_view, mm, header
 gc.collect()
-print(f"({now_hms()}) Fertig: mandelbrot_color.png gespeichert und RAM freigegeben.")
+print(f"({now_hms()}) Fertig: mandelbrot_maxiter.png gespeichert und RAM freigegeben.")
+
